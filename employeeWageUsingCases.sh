beb2e19f2a7c1ff046d157bf1420b8e796222ad4
AshokKoda <a.koda59@gmail.com>
2022-06-01 20:57:21 +0530

    Refactor:UC5:Calculate monthly wage

--- employeeWageUsingCases.sh
+++ employeeWageUsingCases.sh
@@ -3,22 +3,27 @@ FULL_DAY_WORKING_HRS=8
 HALF_DAY_WORKING_HRS=4
 
 dailyWage=0
+MAX_WORKING_DAYS=20
 
-attendance=$((RANDOM%3))
-echo "Random number: $attendance"
+for(( i=0;i<$MAX_WORKING_DAYS;i++))
+do
 
-case $attendance in
-	0)
-		echo "Employee is absent"
-		;;
-	1)
-		echo "Employee is present"
-		dailyWage=$(($WAGE_PER_HOUR * $HALF_DAY_WORKING_HRS))
-		echo "Daily wage for part time: $dailyWage"
-		;;
-	2)
-		echo "EMployee is present"
-		dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_WORKING_HRS))
-		echo "Daily wage for full time: $dailyWage"
-		;;
-esac
+	attendance=$((RANDOM%3))
+
+	case $attendance in
+		0)
+			echo "Employee is absent"
+			echo "Daily wage is 0"
+			;;
+		1)
+			echo "Employee is present"
+			dailyWage=$(($WAGE_PER_HOUR * $HALF_DAY_WORKING_HRS))
+			echo "Daily wage for part time: $dailyWage"
+			;;
+		2)
+			echo "EMployee is present"
+			dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_WORKING_HRS))
+			echo "Daily wage for full time: $dailyWage"
+			;;
+	esac
+done
