3d1ae0261dba6c38ac47539521182886722d2d13
AshokKoda <a.koda59@gmail.com>
2022-06-01 20:14:39 +0530

    Add: UC1: Employee attendance is done

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,1 +1,9 @@
 echo "----------Welcome to employee wage computation------------"
+absent=0
+attendance=$((RANDOM%2))
+if [[ $attendance -eq $absent ]]
+then
+	echo "Employee is absent"
+else
+	echo "EMployee is present"
+fi
