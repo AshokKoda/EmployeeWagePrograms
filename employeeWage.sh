4887bfb331830f80deb38da346ecbe8a6d5f76ef
AshokKoda <a.koda59@gmail.com>
2022-06-01 20:23:05 +0530

    Refactor:UC2:calculate daily wage

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,9 +1,15 @@
 echo "----------Welcome to employee wage computation------------"
+WAGE_PER_HOUR=20
+FULL_DAY_HOURS=8
 absent=0
+
 attendance=$((RANDOM%2))
 if [[ $attendance -eq $absent ]]
 then
 	echo "Employee is absent"
+	dailyWage=0
 else
-	echo "EMployee is present"
+	echo "Employee is present"
+	dailyWage=$(($WAGE_PER_HOUR * $FULL_DAY_HOURS))
+	echo "Daily wage: $dailyWage"
 fi
